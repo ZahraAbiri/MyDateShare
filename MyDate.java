5b7443f489b317b647b5a2918c2ba50a5b4501b8
unknown <narges40jam67@gmail.com>
2021-09-20 21:25:24 +0430

    add validateDate and lastDayOfMonth methods

--- MyDate.java
+++ MyDate.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class MyDate {
     private int year;
     private int month;
@@ -35,4 +38,46 @@ public class MyDate {
 
     public void setDay(int day) {
         this.day = day;
-    }}
+    }
+
+    public boolean isValidDate(int year, int month, int day) {
+        if ((year >= 1 && year <= 9999) && (month >= 1 && month <= 12)) {
+            if (((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) ||
+                    (month == 12)) && (day >= 1) && (day <= 31)) {
+                return true;
+            } else if (((month == 4) || (month == 6) || (month == 9) || (month == 11)) &&
+                    ((day >= 1) && (day <= 30))) {
+                return true;
+            } else if ((month == 2) &&
+                    ((day >= 1) && (day <= 28))) {
+                return true;
+            } else
+                return false;
+        }
+        return false;
+    }
+
+
+    /* public int getMonthLastDay(int month) {
+
+        Calendar cal = Calendar.getInstance();
+        month=month+1;
+        cal.set(month, 1);
+        int dayInt = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+
+        return dayInt;
+    } */
+    public int getMonthLastDay(int month) {
+        int lastDayOfMonth;
+        if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10)) {
+            lastDayOfMonth = 31;
+        } else if ((month == 4) || (month == 6) || (month == 9) || (month == 11)) {
+            lastDayOfMonth = 30;
+
+        } else {
+            lastDayOfMonth = 28;
+        }
+        return lastDayOfMonth;
+
+    }
+}
